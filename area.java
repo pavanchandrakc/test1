a86b18299ded186a6b5115cad69e373c4c9b8d37
pavanchandrakc <pavanchandra2599@gmail.com>
2025-03-17 14:39:55 +0530

    Create area.java

---
+++ area.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.util.Scanner;
+public class area{
+  public static void main(String [] args){
+    Scanner sc=new Scanner(System.in);
+    int r=sc.nextInt();
+    float area=3.14*r*r;
+    System.out.println(area);
+  }
+}
