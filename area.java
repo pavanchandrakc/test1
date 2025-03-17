861e296c9ba402a10a002924224655d2a441d9a8
pavanchandrakc <pavanchandra2599@gmail.com>
2025-03-17 14:47:36 +0530

    Update area.java

--- area.java
+++ area.java
@@ -4,7 +4,7 @@ public class area{
   public static void main(String [] args){
     Scanner sc=new Scanner(System.in);
     int r=sc.nextInt();
-    float area=3.14*r*r;
+    long area=3.14*r*r;
     System.out.println(area);
   }
 }
